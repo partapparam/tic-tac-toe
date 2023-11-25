031a20484885e3fae2873a50d697c848b2a6790b
partapparam <paramsingh1961@gmail.com>
2023-11-24 19:33:25 -0800

    game

--- console.py
+++ console.py
@@ -1,3 +1,24 @@
 from src.Board import *
 from src.Game import *
 from src.Player import *
+
+def play():
+    game = Game()
+    game.print_valid_moves()
+    player = game.player1
+    num = 9
+    while num > 0:
+        num -= 1
+        game.print_board()
+        position = input(f'{player} its your turn')
+
+        game.modify_board(position, player.type)
+
+        if game.won_game(player):
+            print(f'{player} is the winner')
+        else:
+            player = game.change_turn(player)
+    
+    if num == 0:
+        print('Game over')
+
--- src/Player.py
+++ src/Player.py
@@ -1,3 +1,8 @@
 class Player:
-    def __init__(self) -> None:
-        pass
+    # One player
+    def __init__(self, type) -> None:
+        # create a player and give them a type of move = X, O
+        self.type = type 
+    
+    def __str__(self) -> str:
+        return f'Player is of type = {self.type}'
